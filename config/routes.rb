fe7737fa751ace6ea547dce2f338745c4707bd96
AndrewYW <andrewwang39@gmail.com>
2019-01-22 14:31:43 -0500

    finish auth backend

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root "static_pages#root"
 
   namespace :api, defaults: {format: :json} do
-  
+    resources :user, only: [:create]
+    resource :session, only: [:create, :destroy, :show]
   end
 end
