6396e1f7fef7111550fd04b07a62946b88cf38bb
AndrewYW <andrewwang39@gmail.com>
2019-01-22 17:16:31 -0500

    pre heroku config, navbar unfinished

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -3,7 +3,7 @@ import NavContainer from './navbar/navbar_container';
 const App = () => (
   <div>
     <NavContainer />
-    <h1>Harmony App Root</h1>
+    <h1>Harmony App component</h1>
   </div>
 );
 
--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
+library.add(fab);
 
 const Navbar = ({ currentUser }) => {
   const navbar = () => (
     <nav className="Navbar">
       <div className="navLogo">
-        asdfjlkadfs
+        <img src="../../../assets/images/logo.png"/>
       </div>
       <ul className="navLeft">
       
       </ul>
       <ul className="navRight">
-      
+        <a href="https://github.com/AndrewYW/harmony/" className="github">
+          <FontAwesomeIcon icon={['fab','github']} />
+        </a>
       </ul>
     </nav>
   )
