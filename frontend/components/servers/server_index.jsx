c5045ee60c1211fbc0447cbd9c38674da8b2b9b8
AndrewYW <andrewwang39@gmail.com>
2019-01-28 15:57:14 -0500

    update index skeleton

--- frontend/components/servers/server_index.jsx
+++ frontend/components/servers/server_index.jsx
@@ -1,16 +1,35 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 
 class ServerIndex extends React.Component {
 
+  constructor(props) {
+    super(props);
+    // this.state = {}
+  }
   componentDidMount() {
     console.log(this.props);
   }
+
+  componentDidUpdate() {
+    
+  }
   render() {
+    // const servers = this.props.servers
     return (
-      <div>
-        
-        SDFJKLLSJKDF
+      <div className="server-sidebar">
+        <Link to="/channels/@me">
+
+        </Link>
+        <div className="server-split" />
+        <ul className="server-ul">
+          {/* { servers } */}
+        </ul>
+        <button className="server-add-button"></button>
+        <div className="logout-button" onClick={this.props.logout}>
+          +
+        </div>
+        {/* Modal here */}
       </div>
     )
   }
--- frontend/components/servers/server_index_container.jsx
+++ frontend/components/servers/server_index_container.jsx
@@ -1,14 +1,17 @@
 import { connect } from 'redux';
 import ServerIndex from './server_index';
+import { logout } from '../../actions/session_actions';
 import { fetchServers, fetchServer } from '../../actions/server_actions';
 
-const mstp = ({servers}) => ({
-  servers,
+const mstp = ({session, entitites}) => ({
+  userId: session.id,
+  servers: entities.servers,
 })
 
 const mdtp = dispatch => ({
   fetchServers: () => dispatch(fetchServers()),
   fetchServer: id => dispatch(fetchServer(id)),
+  logout: () => dispatch(logout()),
 })
 
 export default connect(mstp, mdtp)(ServerIndex);
